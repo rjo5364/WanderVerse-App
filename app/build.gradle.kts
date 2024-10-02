22ed94cc73d9b369d745481e062cc0205e114dca
daktic <gberg@dakticdigital.com>
2024-10-02 17:59:04 -0400

    Added firebase implementation to build.graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
 }
